da1326ebc183c121623dc2d7551c31d2b67d5e03
David Oliveira <luiz.oliveira@siemens-energy.com>
2022-04-11 16:38:55 -0300

    Adicionando Nodemon a aplicação

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { get } = require('express/lib/response');
 const app= express();
 
 app.get("/",(request,response)=>{
-    return response.send("Hello Word!");
+    return response.json({message: "E aew cambana!!!"});
 })
 
 app.listen(3333);
